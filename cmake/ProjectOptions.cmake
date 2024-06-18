f948ea6c0ba001f19788f3186fffcf7b5219e736
Denys Asauliak <denoming@gmail.com>
2024-06-18 22:15:46 +0300

    add optional nvcodec support

--- cmake/ProjectOptions.cmake
+++ cmake/ProjectOptions.cmake
@@ -1,5 +1,11 @@
 include(FeatureSummary)
 
-# Your options here
+option(RAWENC_ENABLE_NVCODEC "Enable Nvidia codec" OFF)
+if(RAWENC_ENABLE_NVCODEC)
+    list(APPEND VCPKG_MANIFEST_FEATURES "nvc")
+endif()
+add_feature_info(
+    RAWENC_ENABLE_NVCODEC RAWENC_ENABLE_NVCODEC "Build project with Nvidia codec"
+)
 
 feature_summary(WHAT ALL)
